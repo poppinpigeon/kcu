3c5adefd7aa317e8b029bdecc8455b236e727800
popppinpigeon <csia19030@gmail.com>
2023-12-06 22:15:58 -0600

    connected user data with Firebase

---
+++ app/src/main/java/com/cs407/badgerroomie/ReadWriteUserDetails.java
@@ -0,0 +1,11 @@
+package com.cs407.badgerroomie;
+
+public class ReadWriteUserDetails {
+    public String gender, ethnicity, roommate;
+
+    public ReadWriteUserDetails(String textGender, String textEthnicity, String textRoommate){
+        this.gender = textGender;
+        this.ethnicity = textEthnicity;
+        this.roommate = textRoommate;
+    }
+}
--- app/src/main/java/com/cs407/badgerroomie/RegisterActivity.java
+++ app/src/main/java/com/cs407/badgerroomie/RegisterActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -20,6 +21,13 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.Firebase;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -27,6 +35,7 @@ public class RegisterActivity extends AppCompatActivity {
     private ProgressBar progressBar;
 
     private String gender, ethnicity, roommate;
+    private static final String TAG = "RegisterActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +52,18 @@ public class RegisterActivity extends AppCompatActivity {
         adapterGender.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerGender.setAdapter(adapterGender);
 
+        spinnerGender.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                gender = spinnerGender.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //Do nothing
+            }
+        });
+
         //spinner for ethnicity
         Spinner spinnerEthnicity = (Spinner) findViewById(R.id.ethnicity_spinner);
         ArrayAdapter<CharSequence> adapterEthnicity = ArrayAdapter.createFromResource(
@@ -52,6 +73,18 @@ public class RegisterActivity extends AppCompatActivity {
         );
         adapterEthnicity.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerEthnicity.setAdapter(adapterEthnicity);
+
+        spinnerEthnicity.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                ethnicity = spinnerEthnicity.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         //spinner for preferred roommates
         Spinner spinnerRoommate = (Spinner) findViewById(R.id.roommate_spinner);
@@ -63,10 +96,17 @@ public class RegisterActivity extends AppCompatActivity {
         adapterRoommate.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerRoommate.setAdapter(adapterRoommate);
 
-        //store spinner selections
-        gender = spinnerGender.getSelectedItem().toString();
-        ethnicity = spinnerEthnicity.getSelectedItem().toString();
-        roommate = spinnerRoommate.getSelectedItem().toString();
+        spinnerRoommate.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                roommate = spinnerRoommate.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         editTextName = findViewById(R.id.editText_name);
         editTextEmail = findViewById(R.id.editText_email);
@@ -109,12 +149,56 @@ public class RegisterActivity extends AppCompatActivity {
             public void onComplete(@NonNull Task<AuthResult> task) {
                 //go to home activity
                 if(task.isSuccessful()){
-                    Toast.makeText(RegisterActivity.this, "Registered successfully", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(RegisterActivity.this, HomeActivity.class);
-                    //prevents user from returning to register activity when pressing back
-                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                    startActivity(intent);
-                    finish();
+                    FirebaseUser firebaseUser = auth.getCurrentUser();
+
+                    //update display name of user
+                    UserProfileChangeRequest profileChangeRequest = new UserProfileChangeRequest.Builder().setDisplayName(name).build();
+                    firebaseUser.updateProfile(profileChangeRequest);
+
+                    //enter user data to Firebase
+                    ReadWriteUserDetails writeUserDetails = new ReadWriteUserDetails(gender, ethnicity, roommate);
+
+                    //extracting user reference from registered users database
+                    DatabaseReference referenceProfile = FirebaseDatabase.getInstance().getReference("Registered Users");
+                    referenceProfile.child(firebaseUser.getUid()).setValue(writeUserDetails).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(RegisterActivity.this, "Registered successfully. Please verify your email", Toast.LENGTH_SHORT).show();
+
+                                //send verification email
+                                firebaseUser.sendEmailVerification();
+
+                                Intent intent = new Intent(RegisterActivity.this, HomeActivity.class);
+                                //prevents user from returning to register activity when pressing back
+                                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                                finish();
+                            }else{
+                                Toast.makeText(RegisterActivity.this, "Registration failed. Please try again", Toast.LENGTH_SHORT).show();
+                                progressBar.setVisibility(View.GONE);
+                            }
+                        }
+                    });
+
+                }
+                else{
+                    try{
+                        throw task.getException();
+                    }catch (FirebaseAuthWeakPasswordException e){
+                        editTextPassword.setError("Your password is too weak");
+                        editTextPassword.requestFocus();
+                    }catch (FirebaseAuthInvalidCredentialsException e){
+                        editTextEmail.setError("Your email is invalid or already in use");
+                        editTextEmail.requestFocus();
+                    }catch (FirebaseAuthUserCollisionException e){
+                        editTextEmail.setError("User is already registered with this email");
+                        editTextEmail.requestFocus();
+                    }catch (Exception e){
+                        Log.e(TAG, e.getMessage());
+                        Toast.makeText(RegisterActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                    progressBar.setVisibility(View.GONE);
                 }
             }
         });
